38d6307bdb8441c91c084d53224f8a8dbdace27f
Finn Gaughan <107291215+Finn-Gaughan@users.noreply.github.com>
2026-08-24 18:35:26 +1000

    removed starship from fish config because ill only be using fish for my fedora linux setup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,10 +1,26 @@
 # Bitwarden SSH Agent (Cross-platform Linux & macOS)
-if test -S "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
-    set -gx SSH_AUTH_SOCK "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
-else if test -S "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
-    set -gx SSH_AUTH_SOCK "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
-else if test -S "$HOME/Library/Group Containers/24322K3528.com.bitwarden.desktop/t/bitwarden-ssh-agent.sock"
-    set -gx SSH_AUTH_SOCK "$HOME/Library/Group Containers/24322K3528.com.bitwarden.desktop/t/bitwarden-ssh-agent.sock"
+set -l bw_sockets \
+    "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock" \
+    "$HOME/.bitwarden-ssh-agent.sock" \
+    "$HOME/.config/Bitwarden/ssh-agent.sock" \
+    "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock" \
+    "$HOME/Library/Group Containers/24322K3528.com.bitwarden.desktop/t/bitwarden-ssh-agent.sock"
+
+for sock in $bw_sockets
+    if test -S "$sock"
+        set -gx SSH_AUTH_SOCK "$sock"
+        break
+    end
+end
+
+if not set -q SSH_AUTH_SOCK
+    if test -d "$HOME/.var/app/com.bitwarden.desktop"
+        set -gx SSH_AUTH_SOCK "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
+    else if test (uname) = "Darwin"
+        set -gx SSH_AUTH_SOCK "$HOME/Library/Containers/com.bitwarden.desktop/Data/.bitwarden-ssh-agent.sock"
+    else
+        set -gx SSH_AUTH_SOCK "$HOME/.bitwarden-ssh-agent.sock"
+    end
 end
 
 if status is-interactive
